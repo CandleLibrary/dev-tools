c2cd5ae888cd326cd17a2708cbd7ad01e7cabe02
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-06-08 09:42:10 -0600

    Feat: Improve CLI by using @candlelib/paraffin CLI interface
    #changelog
    
    Changed dependency @candlelib/wax to @candlelib/paraffin. Now using
    paraffin to handle CLI process arguments through the new command path
    interface.

--- source/typescript/entry-point/cli.ts
+++ source/typescript/entry-point/cli.ts
@@ -1,16 +1,61 @@
 #! /bin/node
 
-import { getPackageJsonObject, getProcessArgs } from "@candlelib/wax";
+import {
+    getPackageJsonObject,
+    getProcessArgs,
+    addCLIConfig,
+    processCLIConfig
+} from "@candlelib/paraffin";
+import URL from "@candlelib/url";
 import { createDepend, getCandlePackage, validateEligibility } from "../utils/version-sys.js";
 
+const { package: pkg } = await getPackageJsonObject(URL.getEXEURL(import.meta).path);
 
-const args = getProcessArgs({
-    "version": false
+addCLIConfig({
+    key: "root",
+    help_brief: ` 
+CandleLibrary::Dev-Tools v${pkg.version}`,
 });
 
 
-if (args.version) {
+const channel = addCLIConfig("version", {
+    key: "channel",
+    REQUIRES_VALUE: true,
+    validate: (val) => {
+        if (!["release", "beta", "experimental"].includes(val)) {
+            return `Expected value that matched one of these options:\n- release\n- beta\n- experimental`;
+        }
+        return "";
+    },
+    help_brief: `
+Accepted values: release | beta | experimental
 
+The release channel the version should increment to. By default the version 
+is incremented from the most recent version assignment, but a channel can 
+be specified to cause the version to increment INTO the specified channel.`
+});
+
+
+//Versioning CLI Arguments
+addCLIConfig("version", {
+    key: "version",
+    help_brief: ` 
+Increments the version of the Candle Library package based on the 
+the changes made in the package's git repo since the last version
+and on changes made in any of the dependencies that are also 
+Candle Library packages. If any changes have been made in the 
+dependency packages, then their package version will be incremented
+as well, and the dependency specification in the package.json will
+be updated to reflect the dependency's new version. 
+
+This command will not work if any of the affected Candle Library
+packages have uncommitted changes, or if any of the affected packages
+have failing tests. 
+
+This command must be run in the root directory of a Candle Library
+package, additionally, this command will only work with Candle Library 
+packages.`,
+}).callback = (async (args) => {
     const { FOUND, package: pkg, package_dir } = await getPackageJsonObject();
 
     if (FOUND && package_dir == process.cwd() + "/") {
@@ -19,4 +64,10 @@ if (args.version) {
         const dep = await createDepend(pk);
         await validateEligibility(dep);
     }
+});
+
+try {
+    processCLIConfig();
+} catch (e) {
+    process.exit(-1);
 }
